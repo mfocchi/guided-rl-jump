277039a2d666ddd71a4fc4fbaf6ebb6fc7722dd6
jprogrammer13 <buss.riky@gmail.com>
2024-04-09 12:15:29 +0200

    added fall detection for termination

--- source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/jump/jump_env_cfg.py
+++ source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/jump/jump_env_cfg.py
@@ -152,6 +152,11 @@ class TerminationsCfg:
 
     time_out = DoneTerm(func=mdp.time_out, time_out=True)
 
+    base_contact = DoneTerm(
+        func=mdp.illegal_contact,
+        params={"sensor_cfg": SceneEntityCfg("contact_forces", body_names="trunk"), "threshold": 1.0},
+    )
+
 
 @configclass
 class CurriculumCfg:
