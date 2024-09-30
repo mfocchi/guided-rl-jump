fixed jacobian comp, re-enable wbc, fixed force comp, added unilaterality

source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/jump/config/unitree_go1/jump_platform_env_cfg.py
@@ -47,7 +47,7 @@ class UnitreeGo1JumpEnvCfg(LocomotionJumpEnvCfg):
         q_0_lo = torch.tensor([0.2316, -0.2316, 0.2343, -0.2338, 1.3818, 1.3816, 1.6860, 1.6858, -2.4620, -2.4617, -2.3162, -2.3163])
 
         mass_range = 0
-        stiffness_division = 3
+        stiffness_division = 1
 
         self.commands.trunk_target.body_name = trunk_name
         # self.events.add_base_mass.params["asset_cfg"] = SceneEntityCfg("robot", body_names=trunk_name)
@@ -80,7 +80,7 @@ class UnitreeGo1JumpEnvCfg(LocomotionJumpEnvCfg):
         self.events.detect_apex.params["foot_name"] = ".*" + foot_name
         self.events.detect_touchdown.params["sensor_cfg"] = SceneEntityCfg("contact_forces", body_names=".*")
         self.events.detect_fail.params["sensor_cfg"] = SceneEntityCfg("nonfoot_forces", body_names=".*")
-        self.running_rewards.friction_constraint.params["sensor_cfg"] = SceneEntityCfg("contact_forces", body_names=".*")
+        # self.running_rewards.friction_constraint.params["sensor_cfg"] = SceneEntityCfg("contact_forces", body_names=".*")
         self.rewards.target_position_error.params["foot_name"] = ".*" + foot_name
         self.events.physics_material.params["asset_cfg"] = SceneEntityCfg("robot", body_names=".*")
 

source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/jump/mdp/bezier_curve_actions.py
@@ -334,18 +334,15 @@ class BezierCurveAction(ActionTerm):
         rr_foot_pos_b, rr_foot_orient_b = subtract_frame_transforms(root_pose_w[:, 0:3], root_pose_w[:, 3:7], rr_pose_w[:, 0:3], rr_pose_w[:, 3:7])
         rr_foot_pos_des_b, rr_foot_orient_des_b = subtract_frame_transforms(x, o_quat, rr_pose_w[:, 0:3])
 
-        # Orientation is just for visualization (ignore)
-        self.fl_diff_ik_controller.set_command(fl_foot_pos_des_b, ee_quat=fl_foot_orient_des_b)
-        self.fr_diff_ik_controller.set_command(fr_foot_pos_des_b, ee_quat=fr_foot_orient_des_b)
-        self.rl_diff_ik_controller.set_command(rl_foot_pos_des_b, ee_quat=rl_foot_orient_des_b)
-        self.rr_diff_ik_controller.set_command(rr_foot_pos_des_b, ee_quat=rr_foot_orient_des_b)
+        tau_meas = self._asset.data.applied_torque.clone()
 
-        q_des[:, self.fl_entity_cfg.joint_ids] = self.fl_diff_ik_controller.compute(fl_foot_pos_b, fl_foot_orient_b, fl_jacobian, fl_joint_pos)
-        q_des[:, self.fr_entity_cfg.joint_ids] = self.fr_diff_ik_controller.compute(fr_foot_pos_b, fr_foot_orient_b, fr_jacobian, fr_joint_pos)
-        q_des[:, self.rl_entity_cfg.joint_ids] = self.rl_diff_ik_controller.compute(rl_foot_pos_b, rl_foot_orient_b, rl_jacobian, rl_joint_pos)
-        q_des[:, self.rr_entity_cfg.joint_ids] = self.rr_diff_ik_controller.compute(rr_foot_pos_b, rr_foot_orient_b, rr_jacobian, rr_joint_pos)
+        f_fl = (-torch.linalg.pinv(fl_jacobian[:, 0:3].clone().transpose(1, 2)) @ tau_meas[:, self.fl_entity_cfg.joint_ids].unsqueeze(-1))
+        f_fr = (-torch.linalg.pinv(fr_jacobian[:, 0:3].clone().transpose(1, 2)) @ tau_meas[:, self.fr_entity_cfg.joint_ids].unsqueeze(-1))
+        f_rl = (- torch.linalg.pinv(rl_jacobian[:, 0:3].clone().transpose(1, 2)) @ tau_meas[:, self.rl_entity_cfg.joint_ids].unsqueeze(-1))
+        f_rr = (-torch.linalg.pinv(rr_jacobian[:, 0:3].clone().transpose(1, 2)) @ tau_meas[:, self.rr_entity_cfg.joint_ids].unsqueeze(-1))
 
-        qd_des = (q_des - old_q_des) / self.cfg.time_step
+        forces = torch.cat((f_fl, f_fr, f_rl, f_rr), dim=1).reshape(-1,4,3)
+        self._env.extras['forces'] = forces
 
         # compute g com with wbc
         eye_batch = torch.eye(3).repeat(self.num_envs, 1, 1).to(self.device)
@@ -361,8 +358,9 @@ class BezierCurveAction(ActionTerm):
         rr_block = torch.cat((eye_batch, rr_skew), dim=1)
 
         Jb_t = torch.cat((fl_block, fr_block, rl_block, rr_block), dim=2)
-        f = torch.linalg.pinv(Jb_t) @ self.wd.squeeze(1).unsqueeze(2)
+        f = torch.linalg.pinv(Jb_t, atol=1e-4) @ self.wd.squeeze(1).unsqueeze(2)
         f = f.squeeze(-1).view(self.num_envs, 4, 3)
+        # print(f)
 
         fl_tau = (-fl_jacobian[:, 0:3].clone().transpose(1, 2) @ f[:, 0].unsqueeze(-1)).squeeze(-1)
         fr_tau = (-fr_jacobian[:, 0:3].clone().transpose(1, 2) @ f[:, 1].unsqueeze(-1)).squeeze(-1)
@@ -374,6 +372,19 @@ class BezierCurveAction(ActionTerm):
         tau_ff[:, self.fr_entity_cfg.joint_ids] = fr_tau.clone()
         tau_ff[:, self.rl_entity_cfg.joint_ids] = rl_tau.clone()
         tau_ff[:, self.rr_entity_cfg.joint_ids] = rr_tau.clone()
+
+        # Orientation is just for visualization (ignore)
+        self.fl_diff_ik_controller.set_command(fl_foot_pos_des_b, ee_quat=fl_foot_orient_des_b)
+        self.fr_diff_ik_controller.set_command(fr_foot_pos_des_b, ee_quat=fr_foot_orient_des_b)
+        self.rl_diff_ik_controller.set_command(rl_foot_pos_des_b, ee_quat=rl_foot_orient_des_b)
+        self.rr_diff_ik_controller.set_command(rr_foot_pos_des_b, ee_quat=rr_foot_orient_des_b)
+
+        q_des[:, self.fl_entity_cfg.joint_ids] = self.fl_diff_ik_controller.compute(fl_foot_pos_b, fl_foot_orient_b, fl_jacobian, fl_joint_pos)
+        q_des[:, self.fr_entity_cfg.joint_ids] = self.fr_diff_ik_controller.compute(fr_foot_pos_b, fr_foot_orient_b, fr_jacobian, fr_joint_pos)
+        q_des[:, self.rl_entity_cfg.joint_ids] = self.rl_diff_ik_controller.compute(rl_foot_pos_b, rl_foot_orient_b, rl_jacobian, rl_joint_pos)
+        q_des[:, self.rr_entity_cfg.joint_ids] = self.rr_diff_ik_controller.compute(rr_foot_pos_b, rr_foot_orient_b, rr_jacobian, rr_joint_pos)
+
+        qd_des = (q_des - old_q_des) / self.cfg.time_step
 
 
         if not self.cfg.debug_control:
@@ -399,7 +410,7 @@ class BezierCurveAction(ActionTerm):
 
                 q_des[after_t_th_total] = q_0_lo_lerp
                 qd_des[after_t_th_total] = torch.zeros_like(self._asset.data.default_joint_vel[0])
-                
+
                 # filter torque in case of fly or touchdown
                 tau_ff[after_t_th_total] *= self._env.extras['wbc'][after_t_th_total]
 
@@ -424,7 +435,9 @@ class BezierCurveAction(ActionTerm):
 
                 self._env.extras['apex_dt'][apex_env_ids] += self.cfg.time_step
 
-        return q_des, qd_des, tau_ff * 0
+        # print(tau_ff)
+
+        return q_des, qd_des, tau_ff
 
     def map_range(self, x, in_min, in_max, out_min, out_max):
         return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min

source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/jump/mdp/events.py
@@ -50,8 +50,9 @@ def reset_robot_state(
     env.extras['apex_dt'] = torch.zeros(env.num_envs, device=env.device)
     env.extras['apex_z'] = torch.zeros(env.num_envs, device=env.device)
     env.extras['landing_z'] = torch.zeros(env.num_envs, device=env.device)
-    env.extras['wbc'] = torch.zeros((env.num_envs,1), device=env.device)
+    env.extras['wbc'] = torch.zeros((env.num_envs, 1), device=env.device)
     env.extras['fail'] = torch.zeros(env.num_envs, device=env.device, dtype=torch.bool)
+    env.extras['forces'] = torch.zeros((env.num_envs, 4, 3), device=env.device)
 
 
 def reset_landing_platform(
@@ -114,7 +115,6 @@ def detect_apex(
 
     foots_z_pos_w -= foot_height_offset
 
-
     # Get base z linear velocity
     base_lin_vel_w = robot.data.root_state_w[env_ids, 7:10].clone()
 
@@ -161,6 +161,7 @@ def detect_apex(
     landing_platform.write_root_pose_to_sim(torch.cat([positions[existing_apex_ids], orientations[existing_apex_ids]], dim=-1), env_ids=existing_apex_ids)
 
     # print('apex', torch.tensor(list(env.extras['apex'].keys()), device=env.device, dtype=torch.int))
+
 
 def detect_touchdown(env: RLTaskEnv, env_ids: torch.Tensor, contact_threshold: float, sensor_cfg: SceneEntityCfg, asset_cfg: SceneEntityCfg):
     """Terminate when the contact force on the sensor exceeds the force threshold."""

source/extensions/omni.isaac.orbit_tasks/omni/isaac/orbit_tasks/locomotion/jump/mdp/rewards.py
@@ -69,8 +69,6 @@ def target_position_error(env: RLTaskEnv, command_name: str, asset_cfg: SceneEnt
     if mode == "play":
         print(f"Landing td: {curr_pos_w[...,:3]}")
 
-
-
     # Calculate percentual_error to normalize jump performance
     target_z_error = torch.abs(des_pos_w[..., 2] - curr_pos_w[..., 2])
     target_z_error = torch.where(target_z_error <= z_threshold, torch.tensor(0.0), target_z_error)
@@ -231,7 +229,7 @@ def liftoff_angular_velocity_error(env: RLTaskEnv) -> torch.Tensor:
     return torch.norm(des_lo_lvel_w - curr_lo_lvel_w, dim=1)
 
 
-def friction_constraint(env: RLTaskEnv, sensor_cfg: SceneEntityCfg, mu: float = 0.8) -> torch.Tensor:
+def friction_constraint(env: RLTaskEnv, mu: float = 0.8) -> torch.Tensor:
     """Penalize contact forces out of the friction cone
 
     Args:
@@ -239,11 +237,10 @@ def friction_constraint(env: RLTaskEnv, sensor_cfg: SceneEntityCfg, mu: float = 
         mu: the friction coefficient
     """
     # extract the used quantities (to enable type-hinting)
-    contact_sensor: ContactSensor = env.scene.sensors[sensor_cfg.name]
-    net_contact_forces = contact_sensor.data.net_forces_w.clone()
+    net_contact_forces = env.extras["forces"].clone()
 
     # From jumpleg -> residual = np.linalg.norm(self.contactForceW[:2]) - p.mu * p.contactForceW[2]
-    residuals = torch.norm(net_contact_forces[:, sensor_cfg.body_ids, :2], dim=-1) - mu * net_contact_forces[:, sensor_cfg.body_ids, 2]
+    residuals = torch.norm(net_contact_forces[..., :2], dim=-1) - mu * net_contact_forces[..., 2]
 
     # Compute the violation values of friction cone constraint
     #  #evns,#sensors (n, 4)
@@ -265,7 +262,7 @@ def contact_constraint(env: RLTaskEnv, sensor_cfg: SceneEntityCfg, contact_thres
     return costs
 
 
-def unilateral_constraint(env: RLTaskEnv, sensor_cfg: SceneEntityCfg) -> torch.Tensor:
+def unilateral_constraint(env: RLTaskEnv) -> torch.Tensor:
     """Penalize contact forces out of the friction cone
 
     Args:
@@ -273,11 +270,10 @@ def unilateral_constraint(env: RLTaskEnv, sensor_cfg: SceneEntityCfg) -> torch.T
         mu: the friction coefficient
     """
     # extract the used quantities (to enable type-hinting)
-    contact_sensor: ContactSensor = env.scene.sensors[sensor_cfg.name]
-    net_contact_forces = contact_sensor.data.net_forces_w.clone()
+    net_contact_forces = env.extras["forces"].clone()
 
     # sum along each robot to get the total violation cost
-    costs = torch.sum(computeActivationFunction('linear', net_contact_forces[:, sensor_cfg.body_ids, 2], 0.0, torch.inf), dim=1)
+    costs = torch.sum(computeActivationFunction('linear', net_contact_forces[..., 2], 0.0, torch.inf), dim=1)
 
     return costs
 
@@ -292,9 +288,8 @@ def no_touchdown(env: RLTaskEnv) -> torch.Tensor:
     touchdown_end_ids = torch.tensor(list(env.extras['touchdown'].keys()), device=env.device, dtype=torch.int)
     no_touchdown_penalty[touchdown_end_ids] = 0
 
-    
     env.extras['fail_det'] = env.extras['fail'].clone()
-    
+
     return no_touchdown_penalty
 
 